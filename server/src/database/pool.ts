f509a50dee783b5e81545ede6b203897de96aa55
jordanfulawka <98784255+jordanfulawka@users.noreply.github.com>
2026-09-19 14:37:16 -0400

    fix: configure postgres database connection string for deployment

--- server/src/database/pool.ts
+++ server/src/database/pool.ts
@@ -1,7 +1,7 @@
 import pg, { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@localhost:5432/${process.env.DB_NAME}`,
+  connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
 });
 
 export default pool;
